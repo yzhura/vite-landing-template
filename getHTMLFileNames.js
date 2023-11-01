26489595e4fb9de992db1560a52b75064518b13f
yzhura <zhuravlev.yaroslav@gmail.com>
2023-11-01 18:00:41 +0200

    first commit

---
+++ getHTMLFileNames.js
@@ -0,0 +1,10 @@
+import fs from "fs";
+import { resolve } from "path";
+
+const srcDir = resolve(__dirname, 'src');
+
+const htmlFiles = fs
+    .readdirSync(srcDir)
+    .filter((file) => file.endsWith(".html") && file !== "index.html");
+
+export { htmlFiles };
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import { htmlFiles } from './getHTMLFileNames';
+
+const input = { main: resolve(__dirname, "src/index.html") };
+htmlFiles.forEach((file) => {
+  input[file.replace(".html", "")] = resolve(__dirname, 'src', file);
+});
+
+
+export default defineConfig({
+  base: '/vite-deploy-test',
+  root: 'src',
+  publicDir: '../public',
+  plugins: [handlebars({ partialDirectory: resolve(__dirname, "src/templates") })],
+  build: {
+    rollupOptions: {
+      input,
+    },
+    outDir: '../dist/',
+    emptyOutDir: true,
+  },
+});
